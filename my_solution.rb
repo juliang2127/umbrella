06433759472a7aedd89379407be80a90bd65a1c9
juliang2127 <164977539+juliang2127@users.noreply.github.com>
2024-04-08 07:39:01 +0000

    Get current weather to work and geo

--- my_solution.rb
+++ my_solution.rb
@@ -3,8 +3,8 @@ require "json"
 
 pp "Where are you?"
 
-user_location = gets.chomp
-#user_location = "Chicago"
+#user_location = gets.chomp
+user_location = "Chicago"
 pp user_location
 
 map_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
@@ -29,3 +29,21 @@ longitude = location_results.fetch("lng")
 
 pp latitude
 pp longitude
+
+weatherapi_url = "https://api.pirateweather.net/forecast/" + ENV.fetch("PIRATE_WEATHER_KEY")+ "/" + latitude.to_s + "," + longitude.to_s
+
+response_weather = HTTP.get(weatherapi_url)
+
+raw_response_weather = response_weather.to_s
+
+parsed_response_weather = parsed_response = JSON.parse(raw_response_weather)
+
+parsed_response_weather.keys
+
+currently_weather = parsed_response_weather.fetch("currently")
+
+temp_weather = currently_weather.fetch("temperature")
+
+pp temp_weather
+
+
